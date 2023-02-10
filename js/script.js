38a141c3abaa2f8176fb0bfb3450ceeb68b97bec
Koya Ido <Koya@Koya-no-MacBook-Air.local>
2023-02-10 14:13:35 +0900

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-let nowSeconds = new Date().getSeconds();
-console.log(nowSeconds)
+let seconds = Number(document.getElementById('timer').textContent);
+const startButton = document.getElementById('start_button');
+
+function countDown(time){
+  const timerId = setInterval(() => {
+    time --;
+    console.log(time);
+    document.getElementById('timer').textContent = time;
+    if(time <= 0){
+      clearInterval(timerId);
+      document.getElementById('timer').textContent = 0;
+    }
+  },1000);
+};
+
+
+startButton.addEventListener('click', () => {
+  countDown(seconds);
+  setTimeout(function(){
+    alert('終了')
+  }, seconds * 1000 + 1000);
+});
